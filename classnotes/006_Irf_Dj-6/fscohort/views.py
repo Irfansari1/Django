9ea75529cd4eb98e503fc05f5f408bc659ff20d6
Irfansari1 <irfansari42@gmail.com>
2022-02-01 16:10:23 +0100

    class based views_css_js  added

--- classnotes/006_Irf_Dj-6/fscohort/views.py
+++ classnotes/006_Irf_Dj-6/fscohort/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .forms import StudentForm
 from .models import Student
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 # Create your views here.
 
 def home(request):
     return render(request, "fscohort/home.html")
 
+class HomeView(TemplateView):
+    template_name = "fscohort/home.html"
 
 def student_list(request):
 
@@ -17,6 +22,13 @@ def student_list(request):
     }
 
     return render(request, "fscohort/student_list.html", context)
+
+class StudentListView(ListView):
+    # template_name = "fscohort/student_list.html"
+    model = Student
+    context_object_name = 'students'
+
+
 
 def student_add(request):
     form = StudentForm()
@@ -35,6 +47,24 @@ def student_add(request):
 
     return render(request, "fscohort/student_add.html", context)
 
+class StudentCreateView(SuccessMessageMixin, CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "fscohort/student_add.html"
+    success_url = reverse_lazy("list")
+    success_message = "Student added successfully"
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        self.object = form.save()
+        # if not self.object.number:
+        #     self.object.number = 9999
+        self.object.first_name = self.object.first_name.title()
+        self.object.last_name = self.object.last_name.title()
+        self.object.save()
+        return super().form_valid(form)
+
+
 def student_detail(request,id):
     student = Student.objects.get(id=id)
     context = {
@@ -42,6 +72,10 @@ def student_detail(request,id):
     }
 
     return render(request, "fscohort/student_detail.html", context)
+
+class StudentDetailView(DetailView):
+    model = Student
+    pk_url_kwarg = 'id'
 
 def student_update(request, id):
 
@@ -63,6 +97,12 @@ def student_update(request, id):
 
     return render(request, "fscohort/student_update.html", context)
 
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "fscohort/student_update.html"
+    success_url = '/student_list/'
+
 def student_delete(request, id):
 
     student = Student.objects.get(id=id)
@@ -77,3 +117,8 @@ def student_delete(request, id):
         "student":student
     }
     return render(request, "fscohort/student_delete.html",context)
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = "fscohort/student_delete.html"
+    success_url = reverse_lazy("list")
--- classnotes/006_Irf_Dj-6/main/settings.py
+++ classnotes/006_Irf_Dj-6/main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fscohort',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
