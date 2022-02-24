fe3294abb8cb56b8108c3008a2fc71663a3d8652
Irfansari1 <irfansari42@gmail.com>
2022-02-24 16:04:41 +0100

    class based views

---
+++ classnotes/022_Irf_Dj-22_CB-Views/apiTodo/serializers.py
@@ -0,0 +1,21 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    todo_detail = serializers.HyperlinkedIdentityField(view_name='todo-detail')
+
+    class Meta:
+        model = Todo
+        fields = (
+            "id",
+            "todo_detail",
+            "task",
+            "description",
+            "priority",
+            "done"
+        )
+
+# https://www.django-rest-framework.org/api-guide/views/#function-based-views
+
